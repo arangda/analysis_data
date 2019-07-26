8e860a34ccdb0c6d4ac0b38750118454bf085e56
arangda <273890638@qq.com>
2019-07-26 18:34:31 +0800

    1834

--- app/file.py
+++ app/file.py
@@ -13,8 +13,12 @@ from pandas.api.types import is_numeric_dtype
 class file(object):
 
     def __init__(self):
+        # 下面列表和uploads中的子目录，及上传表单name值相关
+        self.dirs = ['swt', 'baidu', 'sogou', 'shenma', '360']
         self.date = time.strftime("%Y-%m-%d",time.localtime())
-        self.read_swt = self.read()
+        self.read_swt = self.read_swts()
+        self.error_exist = '<em>文件不存在,请先上传文件</em>'
+        self.timelist = []
 
     def allowed_file(self,filename):
         return '.' in filename and \
@@ -26,34 +30,111 @@ class file(object):
             if os.path.isdir(newdir):
                 file = request.files.get(dr)
                 if file and self.allowed_file(file.filename):
-                    filename = dr + '_' + self.date + '.' + file.filename.rsplit('.', 1)[1]
-                    file.save(os.path.join(newdir, filename))
+                    if dr == 'swt' and file.filename.rsplit('.', 1)[1] != 'xls':
+                        # flash("商务通表的后缀要为xls")
+                        pass
+                    elif dr != 'swt' and file.filename.rsplit('.', 1)[1] != 'csv':
+                        # flash("搜索引擎表的后缀要为csv")
+                        pass
+                    else:
+                        filename = dr + '.' + file.filename.rsplit('.', 1)[1]
+                        file.save(os.path.join(newdir, filename))
+                        self.is_right(newdir, filename, dr)
+
             else:
                 os.mkdir(newdir)
+        if len(set(self.timelist)) != 1:
+            flash("各个表可能不是同一天的")
+
+    def is_right(self,fdir,fname,dir):
+        t = '00-00-00'
+        fpath = os.path.join(fdir, fname)
+        if dir == 'swt':
+            df = pd.read_excel(fpath)
+            stand = ['编号', '开始访问时间', '客人讯息数', '初次访问网址', '名称', '对话类型', '对话类别', '关键词', 'IP定位', 'IP']
+            isright = set(stand).issubset(set(df.iloc[1].tolist()))
+            if not isright:
+                flash("你确定上传的表是商务通表吗？")
+                os.remove(fpath)
+            else:
+                ptime = time.strptime(df.ix[2][1],"%Y/%m/%d %H:%M:%S")
+                t = time.strftime("%Y-%m-%d",ptime)
+                self.timelist.append(t)
+        if dir == 'baidu':
+            df = pd.read_csv(fpath,encoding="GB2312",skiprows=7)
+            stand =['账户', '日期', '推广计划', '展现', '点击', '消费', '平均点击价格', '点击率', '网页转化', '电话转化', '商桥转化', '推广计划ID']
+            isright = set(stand).issubset(set(df.columns.values.tolist()))
+            if not isright:
+                flash("你确定上传的表是百度报表吗？")
+                os.remove(fpath)
+            else:
+                ptime = time.strptime(df.ix[0][1],"%Y-%m-%d")
+                t = time.strftime("%Y-%m-%d",ptime)
+                self.timelist.append(t)
+        if dir == 'sogou':
+            df = pd.read_csv(fpath,encoding="GB2312",skiprows=[1])
+            stand =['编号', '日期', '账户', '推广计划', '消耗', '点击数', '展示数', '点击率', '点击均价', '有消耗词量']
+            isright = set(stand).issubset(set(df.columns.values.tolist()))
+            if not isright:
+                flash("你确定上传的表是搜狗报表吗？")
+                os.remove(fpath)
+            else:
+                ptime = time.strptime(df.ix[0][1].split("至")[0],"%Y-%m-%d")
+                t = time.strftime("%Y-%m-%d",ptime)
+                self.timelist.append(t)
+        if dir == 'shenma':
+            df = pd.read_csv(fpath,encoding="GB2312")
+            stand =['时间', '账户', '推广计划', '展现量', '点击量', '消费', '点击率', '平均点击价格']
+            isright = set(stand).issubset(set(df.columns.values.tolist()))
+            if not isright:
+                flash("你确定上传的表是神马报表吗？")
+                os.remove(fpath)
+            else:
+                ptime = time.strptime(df.ix[0][0],"%Y/%m/%d")
+                t = time.strftime("%Y-%m-%d",ptime)
+                self.timelist.append(t)
+
+        if dir == '360':
+            df = pd.read_csv(fpath,encoding="GB2312")
+            stand =['日期', '推广账户', '产品线', '推广计划', '展示次数', '点击次数', '点击率', '总费用', '平均每次点击费用']
+            isright = set(stand).issubset(set(df.columns.values.tolist()))
+            if not isright:
+                flash("你确定上传的表是360报表吗？")
+                os.remove(fpath)
+            else:
+                ptime = time.strptime(df.ix[0][0].split("至")[0].strip(),"%Y-%m-%d")
+                t = time.strftime("%Y-%m-%d",ptime)
+                self.timelist.append(t)
+
+        filename = dir + '_' + t + '.' + fname.rsplit('.', 1)[1]
+        newfpath = os.path.join(fdir,filename)
+        os.rename(fpath,newfpath)
 
     def walk(self, dirs):
         dt = {}
         for dr in dirs:
             newdir = os.path.join(app.config['UPLOAD_FOLDER'], dr+'/')
             if os.path.isdir(newdir):
-                dt[dr] = os.listdir(newdir)
+                #此处子判断觉得也许可以更好解决
+                if os.listdir(newdir):
+                    dt[dr] = os.listdir(newdir)
+                else:
+                    dt[dr] = [self.error_exist]
             else:
                 os.mkdir(newdir)
         return dt
+    #读取每个文件前先判断是否存在
     def if_file(self,name):
-        #
         filepath = UPLOAD_FOLDER + name+'/'
         if os.listdir(filepath):
-            return UPLOAD_FOLDER + name+'/' + os.listdir(filepath)[0]
+            file = UPLOAD_FOLDER + name+'/' + os.listdir(filepath)[0]
+            return {"filepath":file}
         else:
-            #flash(name + '文件找不到')
-            return "none"
-        #return filepath
-    def read(self):
-        oldfile = self.if_file('swt')
-        if oldfile != "none":
-            xlsx = pd.ExcelFile(oldfile)
-            df = pd.read_excel(xlsx, 'sheet1', usecols="D,E,F,G", skiprows=[0, 1, 2], names=['D', 'E', 'F', 'G'])
+            return {"error":"不存在这个文件"}
+    def read_swts(self):
+        res = self.if_file('swt')
+        if 'filepath' in res:
+            df = pd.read_excel(res['filepath'], sheet_name=0, usecols="D,E,F,G", skiprows=[0, 1, 2], names=['D', 'E', 'F', 'G'])
             df['engine'] = df.apply(self.what_engine, axis=1)
             df['zhuan'] = df.apply(self.is_zhuan, axis=1)
             df['account'] = df.apply(self.what_account, axis=1)
@@ -64,10 +145,10 @@ class file(object):
                 if ac not in newdf.index:
                     newdf.loc[ac] = [0,0]
             newdf.loc['商务通汇总'] = newdf[['D','zhuan']].apply(lambda x: x.sum())
-            print(newdf)
             return newdf
         else:
-            return "no"
+            return res
+
 
     def is_zhuan(self,data):
         zc = str(data['G'])
@@ -121,9 +202,8 @@ class file(object):
             return '美柚'
 
     def read_baidu(self):
-        oldfile = UPLOAD_FOLDER + 'baidu/' + 'baidu.csv'
-        #xlsx = pd.ExcelFile(oldfile)
-        df = pd.read_csv(oldfile, encoding="GB2312", skiprows=7, usecols=[0,2,3,4,5])
+        res = self.if_file('baidu')
+        df = pd.read_csv(res['filepath'], encoding="GB2312", skiprows=7, usecols=[0,2,3,4,5])
         df['展现'] = pd.to_numeric(df['展现'], errors='coerce')
         df['点击'] = pd.to_numeric(df['点击'], errors='coerce')
         df['消费'] = pd.to_numeric(df['消费'], errors='coerce')
@@ -131,12 +211,13 @@ class file(object):
         newdf = df.groupby('账户',as_index=False).agg({'展现':np.sum,'点击':np.sum,'消费':np.sum})
         newdf[['对话', '转出']] = newdf.apply(self.newcols, args=('账户',), axis=1)
         newdf.loc['百度汇总'] = newdf[['展现', '点击','消费','对话', '转出']].apply(lambda x: x.sum())
+
         newdf = newdf.fillna(0)
         return newdf
 
     def read_sogou(self):
-        oldfile = UPLOAD_FOLDER + 'sogou/' + 'sogou.csv'
-        df = pd.read_csv(oldfile, encoding="GB2312", skiprows=[1], usecols=[2,3,4,5,6],names=['账户','计划','消费', '点击','展现'])
+        res = self.if_file('sogou')
+        df = pd.read_csv(res['filepath'], encoding="GB2312", skiprows=[1], usecols=[2,3,4,5,6],names=['账户','计划','消费', '点击','展现'])
         df = df[~df['账户'].str.contains('账户')]
         df = df[~df['账户'].str.contains('--')]
         #print(df)
@@ -147,13 +228,13 @@ class file(object):
         newdf = df.groupby('账户',as_index=False).agg({'展现':np.sum,'点击':np.sum,'消费':np.sum})
         newdf[['对话', '转出']] = newdf.apply(self.newcols, args=('账户',), axis=1)
         newdf.loc['搜狗汇总'] = newdf[['展现', '点击','消费','对话', '转出']].apply(lambda x: x.sum())
+        print(newdf.info())
         newdf = newdf.fillna(0)
-        print(newdf)
         return newdf
 
     def read_shenma(self):
-        oldfile = UPLOAD_FOLDER + 'shenma/' + 'shenma.csv'
-        df = pd.read_csv(oldfile, encoding="GB2312", skiprows=0, usecols=[1,2,3,4,5],names=['账户','计划','展现','点击','消费'])
+        res = self.if_file('shenma')
+        df = pd.read_csv(res['filepath'], encoding="GB2312", skiprows=0, usecols=[1,2,3,4,5],names=['账户','计划','展现','点击','消费'])
         df = df[~df['账户'].str.contains('账户')]
         df['展现'] = pd.to_numeric(df['展现'], errors='coerce')
         df['点击'] = pd.to_numeric(df['点击'], errors='coerce')
@@ -168,8 +249,8 @@ class file(object):
         return newdf
 
     def read_360(self):
-        oldfile = UPLOAD_FOLDER + '360/' + '360.csv'
-        df = pd.read_csv(oldfile, encoding="GB2312", skiprows=0, usecols=[1,3,4,5,7],names=['账户','计划','展现','点击','消费'])
+        res = self.if_file('360')
+        df = pd.read_csv(res['filepath'], encoding="GB2312", skiprows=0, usecols=[1,3,4,5,7],names=['账户','计划','展现','点击','消费'])
         df = df[~df['账户'].str.contains('推广账户')]
         df['展现'] = pd.to_numeric(df['展现'], errors='coerce')
         df['点击'] = pd.to_numeric(df['点击'], errors='coerce')
@@ -198,5 +279,6 @@ if __name__ == '__main__':
     #hz.loc['大汇总'] = hz[['展现', '点击', '消费', '对话', '转出']].apply(lambda x: x.sum())
     #hz = hz.fillna('0')
     #print(hz)
-    fm = f.if_file('swt')
-    print(fm)
+    #fm = f.read_swt
+    #print(fm)
+    f.upload(['baidu'])
--- app/views.py
+++ app/views.py
@@ -20,13 +20,20 @@ def allowed_file(filename):
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     f = file()
-    # 下面列表和uploads中的子目录，及上传表单name值相关
-    dirs = ['swt', 'baidu', 'sogou', 'shenma', '360']
-    dt = f.walk(dirs)
-    if request.method == 'POST':
-        f.upload(dirs)
-        return redirect(url_for('index'))
+    dt = f.walk(f.dirs)
+    msg = "先上传商务通和各搜索引擎的导出数据"
 
+    if request.method == 'POST':
+        if request.form.get('analysis'):
+            is_swt = True if dt['swt'][0] == f.error_exist else False
+            if is_swt:
+                msg = "没有商务通数据哦"
+            else:
+                return redirect(url_for('data'))
+        elif request.form.get('upload'):
+            f.upload(f.dirs)
+            return redirect(url_for('index'))
+    flash(msg)
     return render_template('index.html',
                            now=int(time.time()),
                            dt=dt
@@ -38,11 +45,38 @@ def index():
 @app.route('/data', methods=['GET', 'POST'])
 def data():
     f = file()
-    baidu = f.read_baidu()
-    sogou = f.read_sogou()
-    shenma = f.read_shenma()
-    d360 = f.read_360()
-    dswt = f.read_swt
+    dt = f.walk(f.dirs)
+    is_swt = True if dt['swt'][0] == f.error_exist else False
+    is_baidu = True if dt['baidu'][0] == f.error_exist else False
+    is_sogou = True if dt['sogou'][0] == f.error_exist else False
+    is_shenma = True if dt['shenma'][0] == f.error_exist else False
+    is_360 = True if dt['360'][0] == f.error_exist else False
+    #如果没有上传商务通表，则跳转首页
+    if is_swt:
+        return redirect(url_for('index'))
+    else:
+        dswt = f.read_swt
+    #如果没有上传搜索引擎的表，则自定义一个dataframe
+    if is_baidu:
+        baidu = pd.DataFrame({'账户':'0','展现':0.0, '点击':0.0,'消费':0.0,'对话':0.0, '转出':0.0},index=['百度汇总'])
+    else:
+        baidu = f.read_baidu()
+
+    if is_sogou:
+        sogou = pd.DataFrame({'账户':'0','展现':0.0, '点击':0.0,'消费':0.0,'对话':0.0, '转出':0.0},index=['搜狗汇总'])
+    else:
+        sogou = f.read_sogou()
+
+    if is_shenma:
+        shenma = pd.DataFrame({'账户':'0','展现':0.0, '点击':0.0,'消费':0.0,'对话':0.0, '转出':0.0},index=['神马汇总'])
+    else:
+        shenma = f.read_shenma()
+
+    if is_360:
+        d360 = pd.DataFrame({'账户':'0','展现':0.0, '点击':0.0,'消费':0.0,'对话':0.0, '转出':0.0},index=['360汇总'])
+    else:
+        d360 = f.read_360()
+
     hz = pd.DataFrame([baidu.loc['百度汇总'],sogou.loc['搜狗汇总'],shenma.loc['神马汇总'],d360.loc['360汇总']])
     hz.loc['大汇总'] = hz[['展现', '点击', '消费', '对话', '转出']].apply(lambda x: x.sum())
     hz = hz.fillna('0')
